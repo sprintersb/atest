d7d342bf47e8aad9ea4f2e034a1629b7d0297730
sprintersb <sprintersb@github>
2014-09-15 12:13:09 +0000

    * COPYING: New file. 	* avrtest.c: Update GPL copyright years. 	* avr-insn.def: Update GPL copyright years. 	* gen-flag-tables.c: Update GPL copyright years. 	(main): Transfer GPL to flag-tables.c. 	* flag-tables.c: Regenerate.

--- avrtest.c
+++ avrtest.c
@@ -1,27 +1,25 @@
 /*
- ****************************************************************************
- *
- * avrtest - A simple simulator for the Atmel AVR family of microcontrollers
- *           designed to test the compiler
- * Copyright (C) 2001, 2002, 2003   Theodore A. Roth, Klaus Rudolph
- * Copyright (C) 2007 Paulo Marques
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- ****************************************************************************
- */
+  This file is part if avrtest -- A simple simulator for the
+  Atmel AVR family of microcontrollers designed to test the compiler.
+
+  Copyright (C) 2001, 2002, 2003   Theodore A. Roth, Klaus Rudolph
+  Copyright (C) 2007 Paulo Marques
+  Copyright (C) 2008-2014 Free Software Foundation, Inc.
+   
+  avrtest is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2,  of the License, or
+  (at your option) any later version.
+  
+  avrtest is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with avrtest; see the file COPYING.  If not, write to
+  the Free Software Foundation, 59 Temple Place - Suite 330,
+  Boston, MA 02111-1307, USA.  */
 
 #include <stdio.h>
 #include <stdlib.h>
